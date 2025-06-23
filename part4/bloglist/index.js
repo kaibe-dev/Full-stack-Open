47b7f54040e0d5033cb52bda50787ce5d5640236
Kalle Alasaarela <kalle.alasaarela@gmail.com>
2025-06-23 17:12:38 +0300

    create util/config.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+const config = require('./utils/config')
 const express = require('express')
 const mongoose = require('mongoose')
 
@@ -12,9 +12,7 @@ const blogSchema = mongoose.Schema({
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(express.json())
 
@@ -34,7 +32,6 @@ app.post('/api/blogs', (request, response) => {
   })
 })
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
